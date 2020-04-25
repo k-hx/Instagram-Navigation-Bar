eaeb3732da9ac86613d92418d30f126a96e1fcb0
k-hx <61220081+k-hx@users.noreply.github.com>
2020-04-25 20:46:10 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -1,23 +1,6 @@
-//the unordered list
 let mainNav = document.getElementById('js-menu');
-
-//the hamburger three lines bar
 let navBarToggle = document.getElementById('js-navbar-toggle');
 
 navBarToggle.addEventListener('click', function() {
     mainNav.classList.toggle('active');
 });
-
-/* DR.SIM'S CODE
-----------------
-let mainNav = document.getElementById('js-menu');
-
-let navBarToggle = document.getElementById('js-navbar-toggle');
-
-navBarToggle.addEventListener('click', function()
-{
-	mainNav.classList.toggle('active');
-});
-*/
-
-/* Make sure the variable name and the id is always right!!!*/
